642c5ea5482d0a939a3176af02c5500ae058fb41
flatlandnick <nicholas.lunn@flatironschool.com>
2021-08-04 09:08:11 -0500

    commit tree 65554d171e3b60d9ab4e0124cf2c393bd2e4b5eb

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,9 +1,9 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  before_action :authorize
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    render json: articles.to_json, each_serializer: ArticleListSerializer
   end
 
   def show
@@ -13,8 +13,12 @@ class MembersOnlyArticlesController < ApplicationController
 
   private
 
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized
+  end
+  
   def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+    render json: { error: "Article not found" }, status: :not_found unless session.include? :user_id
   end
 
 end
